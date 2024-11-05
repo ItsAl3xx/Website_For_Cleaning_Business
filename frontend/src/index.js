b339d0084c2eed97ba0d5180a2086ea314f7fb03
ItsAl3xx <alexbrav0@icloud.com>
2024-11-05 14:07:36 -0800

    Added new files for pages and renamed hompage file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './homepage/App';
+import App from './home-page/App';
 import reportWebVitals from './reportWebVitals';
 
 
